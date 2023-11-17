cced35d313e30ea765708dfcf9b6efe2be8c36e0
Max Petersson <max.petersson@gmail.com>
2023-11-17 14:28:42 +0100

    Joined in on GoodBye world!

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-let message: string = 'TypeScript: Hello, World!';
+let message: string = 'TypeScript: GoodBye, World!';
 // create a new heading 1 element
 let heading = document.createElement('h1');
 heading.textContent = message;
