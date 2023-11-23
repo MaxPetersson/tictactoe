fdd8c74a049fbd44d62031353d70ca1f2988c4b0
Oscar Thureborn <oscar@Oscars-MacBook-Air.local>
2023-11-23 17:39:39 +0100

    adding my first button

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,15 @@ let heading = document.createElement('h1');
 heading.textContent = message;
 // add the heading the document
 document.body.appendChild(heading);
+
+function generateRandomNumber(): string {
+    return "4";
+};
+
+function rollDie(dieParagraphId: string): void {
+        let dingdong = document.getElementById(dieParagraphId);
+        if(dingdong)
+        {
+            dingdong.textContent = generateRandomNumber();
+        }
+};
