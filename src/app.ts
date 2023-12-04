726c25ef262c4d7cf3baf1af4cb33f9e39e2335d
Oscar Thureborn <oscar@Oscars-MacBook-Air.local>
2023-12-05 00:27:46 +0100

    adding buttons with typescript

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,26 @@ heading.textContent = message;
 // add the heading the document
 document.body.appendChild(heading);
 
-function generateRandomNumber(): string {
-    return "4";
+function generateRandomNumber(maximumDieRoll: number): string {
+    const minimumDieRoll = 1;
+    const dieResult = Math.floor(Math.random() * (maximumDieRoll - minimumDieRoll + 1)) + minimumDieRoll;
+    return dieResult as unknown as string;
 };
 
-function rollDie(dieParagraphId: string): void {
-        let dingdong = document.getElementById(dieParagraphId);
-        if(dingdong)
+function delay(milliseconds: number){
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
+};
+
+async function rollDie(dieParagraphId: string, maximumDieRoll: number): Promise<void> {
+        let dieParagraph = document.getElementById(dieParagraphId);
+        if(dieParagraph)
         {
-            dingdong.textContent = generateRandomNumber();
+            for(let i = 0; i < 15; i++)
+            {
+                await delay(100);
+                dieParagraph.innerHTML = generateRandomNumber(maximumDieRoll);
+            }
         }
 };
