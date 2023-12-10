1f0bf6d9c3a34348a364c255b617219498ed9af6
Oscar Thureborn <oscar@Oscars-MacBook-Air.local>
2023-12-10 23:23:51 +0100

    Moved buttons to its own page & folers and created game board page & forlers

---
+++ dice/src/diceScripts.ts
@@ -0,0 +1,23 @@
+function generateRandomNumber(maximumDieRoll: number): string {
+    const minimumDieRoll = 1;
+    const dieResult = Math.floor(Math.random() * (maximumDieRoll - minimumDieRoll + 1)) + minimumDieRoll;
+    return dieResult as unknown as string;
+};
+
+function delay(milliseconds: number){
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
+};
+
+async function rollDie(dieParagraphId: string, maximumDieRoll: number): Promise<void> {
+        let dieParagraph = document.getElementById(dieParagraphId);
+        if(dieParagraph)
+        {
+            for(let i = 0; i < 15; i++)
+            {
+                await delay(100);
+                dieParagraph.innerHTML = generateRandomNumber(maximumDieRoll);
+            }
+        }
+};
--- src/app.ts
+++ src/app.ts
@@ -4,27 +4,3 @@ let heading = document.createElement('h1');
 heading.textContent = message;
 // add the heading the document
 document.body.appendChild(heading);
-
-function generateRandomNumber(maximumDieRoll: number): string {
-    const minimumDieRoll = 1;
-    const dieResult = Math.floor(Math.random() * (maximumDieRoll - minimumDieRoll + 1)) + minimumDieRoll;
-    return dieResult as unknown as string;
-};
-
-function delay(milliseconds: number){
-    return new Promise(resolve => {
-        setTimeout(resolve, milliseconds);
-    });
-};
-
-async function rollDie(dieParagraphId: string, maximumDieRoll: number): Promise<void> {
-        let dieParagraph = document.getElementById(dieParagraphId);
-        if(dieParagraph)
-        {
-            for(let i = 0; i < 15; i++)
-            {
-                await delay(100);
-                dieParagraph.innerHTML = generateRandomNumber(maximumDieRoll);
-            }
-        }
-};
