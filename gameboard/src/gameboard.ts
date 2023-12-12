2b3313252417d5ecf21be7df38d99569c817176f
Oscar Thureborn <oscar@Oscars-MacBook-Air.local>
2023-12-12 23:01:56 +0100

    add mouseover on game squares and xoxo css

---
+++ gameboard/src/gameboard.ts
@@ -0,0 +1,13 @@
+function setupGameSquare(gameSquare: HTMLElement)
+{
+    gameSquare.addEventListener("mouseover", () => {gameSquare.style.backgroundColor = 'lightsalmon'});
+    gameSquare.addEventListener("mouseleave", () => {gameSquare.style.backgroundColor = 'white'});
+}
+
+function setupGameSquares()
+{
+    const gameSquares = document.getElementsByClassName("gameSquare");
+    const gameSquareArray = Array.from(gameSquares);
+    
+    gameSquareArray.forEach(gameSquare => {setupGameSquare(gameSquare as HTMLElement);});
+}
