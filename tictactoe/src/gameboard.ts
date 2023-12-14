ede0db2b83421c7dcda39b7828f6eaf52faa73cf
Oscar Thureborn <oscar@Oscars-MacBook-Air.local>
2023-12-14 21:15:41 +0100

    add helper functions to create x and o elements

--- tictactoe/src/gameboard.ts
+++ tictactoe/src/gameboard.ts
@@ -2,27 +2,65 @@ let nrOfSquares: number = 9;
 let gameSquares: Array<HTMLDivElement> = new Array(nrOfSquares);
 let gameBoard: HTMLDivElement;
 
+function createX(): HTMLDivElement{
+    let xRight = document.createElement('div') as HTMLDivElement
+    xRight.classList.add('crossRight');
+
+    let xLeft = document.createElement('div') as HTMLDivElement
+    xLeft.classList.add('crossLeft');
+
+    let x = document.createElement('div') as HTMLDivElement
+    x.classList.add('symbolContainer');
+    x.appendChild(xRight);
+    x.appendChild(xLeft);
+    x.style.visibility = 'hidden';
+
+    return x;
+}
+
+function createO(): HTMLDivElement{
+    let innerO = document.createElement('div') as HTMLDivElement
+    innerO.classList.add('innerCircle');
+
+    let outerO = document.createElement('div') as HTMLDivElement
+    outerO.classList.add('outerCircle');
+    outerO.appendChild(innerO);
+
+    let o = document.createElement('div') as HTMLDivElement
+    o.classList.add('symbolContainer');
+    o.appendChild(outerO);
+    o.style.visibility = 'hidden';
+
+    return o;
+}
+
 function createGameSquare(): HTMLDivElement {
     let gameSquare = document.createElement('div') as HTMLDivElement
     gameSquare.classList.add('gameSquare');
 
+    // X's and O's
+    let o = createO();
+    gameSquare.appendChild(o);
+
+    let x = createX()
+    gameSquare.appendChild(x);
+
+
     // Mouseover
-    gameSquare.addEventListener("mouseover", () => { gameSquare.style.backgroundColor = 'lightsalmon' });
-    gameSquare.addEventListener("mouseleave", () => { gameSquare.style.backgroundColor = 'white' });
+    gameSquare.addEventListener("mouseover", () => { 
+        gameSquare.style.backgroundColor = 'lightsalmon';
+        (o.getElementsByClassName('innerCircle')[0] as HTMLDivElement).style.backgroundColor = 'lightsalmon';
+    });
+    gameSquare.addEventListener("mouseleave", () => {
+        gameSquare.style.backgroundColor = 'white';
+        (o.getElementsByClassName('innerCircle')[0] as HTMLDivElement).style.backgroundColor = 'white';
+    });
 
     // Click
-    gameSquare.addEventListener("click", () => { squareX.style.visibility = 'visible'; });
-
-    // X's and O's
-    let squareO = document.createElement('div') as HTMLDivElement
-    squareO.classList.add('outerCircle');
-    squareO.style.visibility = 'hidden';
-    gameSquare.appendChild(squareO);
-
-    let squareX = document.createElement('div') as HTMLDivElement
-    squareX.classList.add('crossRight');
-    squareX.style.visibility = 'hidden';
-    gameSquare.appendChild(squareX);
+    gameSquare.addEventListener("click", () => { 
+        o.style.visibility = 'visible';
+        x.style.visibility = 'visible';
+    });
 
     return gameSquare;
 }
