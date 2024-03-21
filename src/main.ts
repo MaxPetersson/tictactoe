a7a667bc0a1abb7da72fac8c20867b6a3119f9a0
Max Petersson <max.petersson@gmail.com>
2024-03-21 20:01:04 +0100

    make main script work and remove unnecessary things from it

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-let message: string = 'TypeScript: GoodBye, World!';
-// create a new heading 1 element
-let heading = document.createElement('h1');
-heading.textContent = message;
-// add the heading the document
-document.body.appendChild(heading);
+function init() {
+    console.log("main.js loaded");
+}
+
+window.onload = init;
